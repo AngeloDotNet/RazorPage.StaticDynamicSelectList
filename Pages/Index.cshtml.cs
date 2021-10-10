3702ecf830ef8f7f81f440bbd8161494ddd23fbd
Angelo Pirola <angelo9871@gmail.com>
2021-10-10 22:55:20 +0200

    Refactoring della Pages Index

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,12 +1,10 @@
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using SelectListMvc_Load_Static_Dynamic.Models.Entities;
 using SelectListMvc_Load_Static_Dynamic.Models.Enums;
+using SelectListMvc_Load_Static_Dynamic.Models.Services.Application;
 using SelectListMvc_Load_Static_Dynamic.Models.Services.Infrastructure;
 using SelectListMvc_Load_Static_Dynamic.Models.ViewModels;
 
@@ -23,17 +21,21 @@ namespace SelectListMvc_Load_Static_Dynamic.Pages
         public Utenti Utenti { get; set; }
 
         public SelectList UsersList { get; set; }
+        public SelectList PeopleList { get; set; }
 
-        public async Task<IActionResult> OnGetAsync()
+        public ListViewModel<UserViewModel> Users { get; set; }
+
+        public async Task<IActionResult> OnGetAsync([FromServices] IUserService userService)
         {
-            IQueryable<User> queryLinq = dbContext.Utenti
-                .AsNoTracking();
-            
-            List<HomeViewModel> listUsers = await queryLinq
-                .Select(user => HomeViewModel.FromEntity(user))
-                .ToListAsync();
+            List<UserViewModel> listUsers = await userService.GetUsersFromDatabase();
+
+            List<UserViewModel> listPeople = await userService.GetUsersFromDatabase();
 
             UsersList = new SelectList(listUsers, "Id", "Nominativo");
+
+            PeopleList = new SelectList(listPeople, "Id", "Nominativo");
+
+            Users = await userService.GetListaUtenti();
             
             return Page();
         }
